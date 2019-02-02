34b10a08aee3ebd24922b572e413af5360ad89d3
tatsuyaHello <ryuuma1005@gmail.com>
2019-02-02 14:15:35 +0900

    Added pg gem and updated Gemfile.lock change

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :questions, only: [:new, :create]
-
+  root 'questions#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
